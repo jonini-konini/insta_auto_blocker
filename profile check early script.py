5de14d478530921c4f9e9123e217c1a6e333b0b2
Sishir Chilakamarri <chilakam@purdue.edu>
2025-09-21 09:58:25 -0400

    Update profile check early script.py
    Adds 'handle' variable for better presentation

--- profile check early script.py
+++ profile check early script.py
@@ -1,4 +1,6 @@
 import json
+
+handle = input("Enter user handle: )
 def profile_check():
     block_factor = 0
     pfp = input("does the person have a profile picture? y/n:")
@@ -27,19 +29,19 @@ def profile_check():
         if dm == "DM for collabs ":
             block_factor += 20
     if block_factor >= 150:
-        print("blocked")
+        print("Account: " + handle + " is blocked.")
     elif block_factor < 150 and block_factor >= 100:
         creation_time = int(input("how many days ago was the account created?:"))
         if creation_time < 10:
-            print("blocked")
+            print("Account: " + handle + " is blocked.")
         elif creation_time >= 10:
             similar_acc = input("does the person have a similar name/pfp to another account? y/n:")
             if similar_acc == "y":
-                print("blocked")
+                print("Account: " + handle + " is blocked.")
             elif similar_acc == "n":
-                print("allowed")
+                print("Account: " + handle + " is allowed.")
     else:
-        print("allowed")
+        print("Account: " + handle + " is allowed.")
 
 
     
