c1301889c6fb51f3e202deb8a1d9565dda6bc00c
jonini-konini <jjon79649@email.com>
2025-09-21 11:56:15 -0400

    final

--- profile check early script.py
+++ profile check early script.py
@@ -5,36 +5,32 @@ df = pd.read_csv('profile_list.csv')
 blocked_terms = open('BlockedBios.txt', 'r', encoding="utf-8").read().split(",")
 
 def profile_check():
-    handle = df[[df['Handle']]]
-    for row in df.iterrows():
-        if row["PFP"] == 'No':
+    block_factor = 0
+    handle = df.set_index("Handle")
+    for i in df.itertuples():
+        if i[7].find("No") != -1:
          block_factor += 50
-        num_follewers = df[[df['Followers']]]
-        num_following = df[[df['Following']]]
+        num_follewers = i[4]
+        num_following = i[5]
         follow_ratio = num_follewers / num_following
         if follow_ratio < 0.1:
             block_factor += 50
         elif follow_ratio >= 0.1 and follow_ratio < 0.3:
             block_factor += 20
-        bio = df[[df['BIO']]]
-        for term in blocked_terms:
-            if term + " " in bio or " " + term in bio:
-                block_factor += 40
 
-        if block_factor >= 110:
+        if block_factor >= 80:
             print("blocked")
-        elif block_factor < 110 and block_factor >= 60:
-            creation_time = df[[df['Days Since Created']]]
-        if creation_time < 10:
-            print("Account: " + handle + " is blocked.")
-        elif creation_time >= 10:
-            duplicate_rows = df[df.duplicated(subset=['User Name'], keep=False)]
-            if not duplicate_rows.empty:
+        elif block_factor < 80 and block_factor >= 60:
+            creation_time = i[9]
+            if creation_time < 10:
                 print("Account: " + handle + " is blocked.")
+            elif creation_time >= 10:
+                duplicate_rows = df[df.duplicated(subset=['User Name'], keep=False)]
+                if not duplicate_rows.empty:
+                 print("Account: " + handle + " is blocked.")
             else :
                 print("Account: " + handle + " may require further inspection.")                                                                        
         else:
             print("allowed")
 
-
-print(df)
+profile_check()
